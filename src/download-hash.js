a9d1b4e070fa13263d4fc743087fca5573e32c10
Henrique Dias <hacdias@gmail.com>
2020-04-17 22:14:02 +0100

    wip: improve download cid

--- src/download-hash.js
+++ src/download-hash.js
@@ -1,13 +1,15 @@
-const path = require('path')
+const { join } = require('path')
 const fs = require('fs-extra')
 const i18n = require('i18next')
+const isIPFS = require('is-ipfs')
 const { clipboard, app, shell } = require('electron')
 const logger = require('./common/logger')
 const { IS_MAC } = require('./common/consts')
-const { notify, notifyError } = require('./common/notify')
 const setupGlobalShortcut = require('./setup-global-shortcut')
 const { selectDirectory } = require('./dialogs')
 const dock = require('./dock')
+const showPrompt = require('./prompt')
+const { showDialog } = require('./dialogs')
 
 const CONFIG_KEY = 'downloadHashShortcut'
 
@@ -16,25 +18,51 @@ const SHORTCUT = IS_MAC
   : 'CommandOrControl+Alt+D'
 
 async function saveFile (dir, file) {
-  const location = path.join(dir, file.path)
+  const location = join(dir, file.path)
   await fs.outputFile(location, file.content)
 }
 
-async function downloadHash (ctx) {
-  const { getIpfsd } = ctx
-  let text = clipboard.readText().trim()
-  const ipfsd = await getIpfsd()
+async function getCID () {
+  const text = clipboard.readText().trim()
 
-  if (!ipfsd || !text) {
+  const { button, input } = await showPrompt({
+    title: i18n.t('downloadCidContentDialog.title'),
+    message: i18n.t('downloadCidContentDialog.message'),
+    placeholder: isIPFS.cid(text) ? text : '',
+    buttons: [
+      i18n.t('downloadCidContentDialog.action'),
+      i18n.t('cancel')
+    ]
+  })
+
+  if (button !== 0) {
     return
   }
 
+  return input
+}
+
+async function downloadHash (ctx) {
+  const cid = await getCID()
+  if (!cid) {
+    return
+  }
+
+  const { getIpfsd } = ctx
+  const ipfsd = await getIpfsd()
+
+  if (!ipfsd) {
+    return
+  }
+
+  let path
   try {
-    text = await ipfsd.api.resolve(text)
+    path = await ipfsd.api.resolve(cid)
   } catch (_) {
-    notify({
-      title: i18n.t('cantDownloadHash'),
-      body: i18n.t('invalidHashClipboard')
+    showDialog({
+      title: i18n.t('cantResolveCidDialog.title'),
+      message: i18n.t('cantResolveCidDialog.message', { path }),
+      buttons: [i18n.t('close')]
     })
 
     return
@@ -45,23 +73,26 @@ async function downloadHash (ctx) {
   }))
 
   if (!dir) {
-    logger.info(`[hash download] dropping hash ${text}: user didn't choose a path.`)
+    logger.info(`[hash download] dropping ${path}: user didn't choose a path.`)
     return
   }
 
   let files
 
   try {
-    logger.info(`[hash download] downloading ${text}: started`, { withAnalytics: 'DOWNLOAD_HASH' })
-    files = await ipfsd.api.get(text)
-    logger.info(`[hash download] downloading ${text}: completed`)
+    logger.info(`[hash download] downloading ${path}: started`, { withAnalytics: 'DOWNLOAD_HASH' })
+    files = await ipfsd.api.get(path)
+    logger.info(`[hash download] downloading ${path}: completed`)
   } catch (err) {
-    logger.error(`[hash download] ${err.toString()}`)
+    logger.error(`[hash download] ${err.stack}`)
 
-    notifyError({
-      title: i18n.t('cantDownloadHash'),
-      body: i18n.t('errorWhileDownloadingHash')
+    showDialog({
+      title: i18n.t('couldNotGetCidDialog.title'),
+      message: i18n.t('couldNotGetCidDialog.message', { path }),
+      buttons: [i18n.t('close')]
     })
+
+    return
   }
 
   try {
@@ -71,18 +102,25 @@ async function downloadHash (ctx) {
         .map(file => saveFile(dir, file))
     )
 
-    notify({
-      title: i18n.t('hashDownloaded'),
-      body: i18n.t('hashDownloadedClickToView', { hash: text })
-    }, () => {
-      shell.showItemInFolder(path.join(dir, files[0].path))
+    const opt = showDialog({
+      title: i18n.t('contentsSavedDialog.title'),
+      message: i18n.t('contentsSavedDialog.message', { path }),
+      buttons: [
+        i18n.t('contentsSavedDialog.action'),
+        i18n.t('close')
+      ]
     })
+
+    if (opt === 0) {
+      shell.showItemInFolder(join(dir, files[0].path))
+    }
   } catch (err) {
     logger.error(`[hash download] ${err.toString()}`)
 
-    notifyError({
-      title: i18n.t('cantDownloadHash'),
-      body: i18n.t('errorWhileWritingFiles')
+    showDialog({
+      title: i18n.t('couldNotSaveDialog.title'),
+      message: i18n.t('couldNotSaveDialog.message'),
+      buttons: [i18n.t('close')]
     })
   }
 }
---
+++ src/prompt/index.js
@@ -0,0 +1,114 @@
+const { BrowserWindow, ipcMain, systemPreferences } = require('electron')
+const crypto = require('crypto')
+const { IS_MAC } = require('../common/consts')
+
+const getId = () => crypto.randomBytes(16).toString('hex')
+
+const isInverse = IS_MAC
+
+function generatePage ({ title, message, placeholder = '', buttons }, id) {
+  buttons = buttons.map((txt, i) => `<button id="${i}">${txt}</button>`)
+
+  if (isInverse) {
+    buttons.reverse()
+  }
+
+  const page = `<!DOCTYPE html>
+<html>
+  <body>
+    <p style="margin: 0">${message}</p>
+    <input type="text" value="${placeholder}" />
+    <div id="buttons">${buttons.join('\n')}</div>
+  </body>
+  <style>
+  * {
+    box-sizing: border-box;
+  }
+  body, html {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-rendering: optimizeLegibility;
+    font-family: -apple-system,BlinkMacSystemFont,
+      avenir next,
+      avenir,
+      helvetica,
+      helvetica neue,
+      ubuntu,
+      roboto,
+      noto,
+      segoe ui,
+      arial,sans-serif;
+    padding: 1rem;
+    color: ${systemPreferences.isDarkMode() ? '#ffffff' : '#000000'};
+    background: ${systemPreferences.isDarkMode() ? '#333333' : '#f7f7f7'};
+  }
+  input {
+    display: block;
+    width: 100%;
+    border: 1px solid rgba(0, 0, 0, 0.05);
+    margin: 1rem 0;
+    padding: 0.25rem;
+    outline: 0;
+  }
+  #buttons {
+    text-align: right;
+  }
+  button {
+    border: 1px solid rgba(0, 0, 0, 0.25);
+    border-radius: 0.2rem;
+    background: #222222;
+    color: white;
+    margin-left: 0.5rem;
+    padding: 0.25rem 0.5rem;
+  }
+  button:last-of-type {
+    background: #0b3a53;
+  }
+  </style>
+  <script>
+    const { ipcRenderer } = require('electron')
+
+    for (const button of document.querySelectorAll('button')) {
+      button.addEventListener('click', event => {
+        ipcRenderer.send('${id}', {
+          input: document.querySelector('input').value,
+          button: Number(button.id)
+        })
+      })
+    }
+  </script>
+</html>`
+
+  return `data:text/html;base64,${Buffer.from(page, 'utf8').toString('base64')}`
+}
+
+module.exports = async function showPrompt (options) {
+  const window = new BrowserWindow({
+    title: options.title,
+    show: false,
+    width: 350,
+    height: 200,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  const id = getId()
+
+  return new Promise(resolve => {
+    ipcMain.once(id, (_, data) => {
+      window.close()
+      resolve(data)
+    })
+
+    window.once('ready-to-show', () => {
+      window.show()
+    })
+
+    window.loadURL(generatePage(options, id))
+  })
+}
